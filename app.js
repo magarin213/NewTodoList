81ab798000d8b34c14af7ffa1b2fa9fb0ec46439
magarin <mario213@nate.com>
2021-09-20 21:45:46 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = loginForm.querySelector("input")
+const greeting = document.querySelector("#greeting")
+
+
+const HIDDEN_CLASSNAME = "hidden"
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+
+
+
+loginForm.addEventListener("submit", onLoginSubmit)
