70c0358bbabce71a28588bfd5c16b43ade7bffe5
magarin <mario213@nate.com>
2021-09-21 01:33:51 +0900

    localstorage getItem, 중복처리 함수로 생성

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ const greeting = document.querySelector("#greeting")
 
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME)
   const username = loginInput.value;
-  localStorage.setItem("username", username)
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreeting(username)
+}
+
+function paintGreeting(username) {
   greeting.innerText = `Hello ${username}`
-  greeting.classList.remove(HIDDEN_CLASSNAME)
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
+const savedUsername = localStorage.getItem(USERNAME_KEY)
 
-
-loginForm.addEventListener("submit", onLoginSubmit)
+if(savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit)
+}else {
+  paintGreeting(savedUsername)
+}
