0653dcbc7d03fcfff98019991be9e660de200488
magarin <mario213@nate.com>
2021-09-22 01:34:19 +0900

    weather api

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ function getClock(params) {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2,"0");
   const second = String(date.getSeconds()).padStart(2,"0");
-  clock.innerText = (`${hours < 10 ? `오전 0${hours}` : `${hours > 12 ? `오후 0${hours - 12}` : `오전 ${hours}`}`}:${minutes}:${second}`)}
+  clock.innerText = (`${hours < 10 ? `오전 ${hours}` : `${hours > 12 ? `오후 0${hours - 12}` : `오전 ${hours}`}`}:${minutes}:${second}`)}
 
 
 getClock();
