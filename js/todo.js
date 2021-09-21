8a1f363f3775410fb14d1bb03951d7b01351fb6f
magarin <mario213@nate.com>
2021-09-21 19:53:13 +0900

    Adding Todos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,21 @@ const toDoform = document.querySelector("#todo-form")
 const toDoInput = toDoform.querySelector("input")
 const toDoList = document.querySelector("#todo-list")
 
+function patinToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li)
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault()
   const newTodo = toDoInput.value;
-  console.log(toDoInput.value)
+
   toDoInput.value = "";
-  console.log(newTodo, "인풋",toDoInput.value)
+
+  patinToDo(newTodo)
 }
 
 
